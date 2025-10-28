0ebe0a51a416ecffa4e2ce1432e226810f831b19
akiptooo <andrew.kiptoo@strathmore.edu>
2025-10-28 19:22:32 +0300

    Configured database

--- judiciaryIS/apps/cms/cms/settings.py
+++ judiciaryIS/apps/cms/cms/settings.py
@@ -73,10 +73,14 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'judiciarydb',
+    'USER': 'postgres',
+    'PASSWORD': 'Rui676758',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
 }
 
 
